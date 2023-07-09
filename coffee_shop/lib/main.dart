843ca81850327bda03c13b26e5f74577eeb4a1b3
TigranHayrapetyan13 <tigran.hayrapetyan17ht@gmail.com>
2023-07-09 16:27:55 +0400

    di

--- coffee_shop/lib/main.dart
+++ coffee_shop/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'my_app.dart';
+import 'presentation/coffe_app.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.setup();
 
-  runApp(const MyApp());
+  runApp(const CoffeeApp());
 }
--- coffee_shop/lib/presentation/coffee/coffe_app.dart
+++ coffee_shop/lib/presentation/coffee/coffe_app.dart
@@ -3,10 +3,10 @@ import 'package:coffe_shop/presentation/coffee/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'application/cubit/coffee_cubit.dart';
+import '../application/cubit/coffee_cubit.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CoffeeApp extends StatelessWidget {
+  const CoffeeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
